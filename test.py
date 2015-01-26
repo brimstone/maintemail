7ba1feb5216bad510c992b83a7f80229562da15e
Matt <brimstone@the.narro.ws>
2015-01-26 18:33:19 -0500

    Figured out how to parse the headings away from the markdown, all in one pass.

--- test.py
+++ test.py
@@ -2,7 +2,6 @@
 from markdown import markdown
 from markdown.extensions import Extension
 from markdown.treeprocessors import Treeprocessor
-from xml.etree.ElementTree import *
 import urllib2
 import vobject
 
@@ -11,31 +10,37 @@ import vobject
 
 class HeaderFinder(Extension):
 
+    def __init__(self):
+        self.headings = []
+        Extension.__init__(self)
+
     def extendMarkdown(self, md, md_globals):
         ext = MyTreeprocessor(md)
         md.treeprocessors.add("toc", ext, "<prettify")
+        self.headings = ext.getHeadings()
+
+    def getHeadings(self):
+        return self.headings
 
 
 # This is called from HeaderFinder()
 class MyTreeprocessor(Treeprocessor):
 
+    # Prepare an array to hold the headings we find
+    def __init__(self, md):
+        self.headings = []
+        Treeprocessor.__init__(self, md)
+
     def run(self, root):
-        # Prepare an array to hold the headings we find
-        headings = []
         # loop through each top level element
         for c in range(len(root) - 1, -1, -1):
             # If it's a h1 or h2, add the text to our heading
             if root[c].tag == "h1" or root[c].tag == "h2":
-                headings.append(root[c].text)
-            # delete the element because we're totally changing our root
-            del root[c]
-        # start our table
-        new_root = SubElement(root, "ul")
-        for h in reversed(headings):
-            item = SubElement(new_root, "li")
-            item.text = h
-        # return our modified root
+                self.headings.append(root[c].text)
         return root
+
+    def getHeadings(self):
+        return self.headings
 
 
 req = urllib2.Request(
@@ -43,10 +48,14 @@ req = urllib2.Request(
 response = urllib2.urlopen(req)
 icalstream = response.read()
 
+# http://vobject.skyhouseconsulting.com/usage.html
 parsedCal = vobject.readOne(icalstream)
 print parsedCal.vevent.dtstart.value
 description = parsedCal.vevent.description.value
 
-systemlist = markdown(description, extensions=[HeaderFinder()])
+headerfinder = HeaderFinder()
+systemdesc = markdown(description, extensions=[headerfinder])
 
-print systemlist
+print systemdesc
+
+print headerfinder.getHeadings()
