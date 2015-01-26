f601bd0dde728846b1f237eb97abac1b0ed96166
Matt <brimstone@the.narro.ws>
2015-01-26 12:26:55 -0500

    Adding comments and playing with autopep8

--- test.py
+++ test.py
@@ -5,24 +5,37 @@ from markdown.treeprocessors import Treeprocessor
 from xml.etree.ElementTree import *
 
 # This is called when markdown() is called on some text
+
+
 class HeaderFinder(Extension):
-	def extendMarkdown(self, md, md_globals):
-		ext = MyTreeprocessor(md)
-		md.treeprocessors.add("toc", ext, "<prettify")
+
+    def extendMarkdown(self, md, md_globals):
+        ext = MyTreeprocessor(md)
+        md.treeprocessors.add("toc", ext, "<prettify")
 
 
 # This is called from HeaderFinder()
 class MyTreeprocessor(Treeprocessor):
+
     def run(self, root):
-		headings = []
-		for c in range(len(root)-1, -1, -1):
-			if root[c].tag == "h1" or root[c].tag == "h2":
-				headings.append(root[c].text)
-			del root[c]
-		root.text = ""
-		for h in reversed(headings):
-			root.text += "* " + h + "\n"
-		return root
+        # Prepare an array to hold the headings we find
+        headings = []
+        # loop through each top level element
+        for c in range(len(root) - 1, -1, -1):
+            # If it's a h1 or h2, add the text to our heading
+            if root[c].tag == "h1" or root[c].tag == "h2":
+                headings.append(root[c].text)
+            # delete the element because we're totally changing our root
+            del root[c]
+        # start our table
+        # This is probably should use etree.Element("ui") correctly
+        root.text = "<ul>"
+        for h in reversed(headings):
+            root.text += "<li>" + h + "</li>"
+        root.text += "</ul>"
+        # return our modified root
+        return root
+
 
 description = '''Github Appliance
 =====
