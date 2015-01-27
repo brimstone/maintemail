61927772d005ca51545b5b62650f5db8879aa68d
Matt <brimstone@the.narro.ws>
2015-01-27 13:06:27 -0500

    Accounting for file rename and adding verbose call.

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
-./test.py --ics https://www.google.com/calendar/ical/uq2m73m8lvm2hf86nbfl9g8gkk%40group.calendar.google.com/private-12556f00fa50f0f4a10c2dcf65d7771f/basic.ics \
+./maintemail.py --ics https://www.google.com/calendar/ical/uq2m73m8lvm2hf86nbfl9g8gkk%40group.calendar.google.com/private-12556f00fa50f0f4a10c2dcf65d7771f/basic.ics \
 	--smtp mail.in.the.narro.ws \
 	--template maint.html \
-    --from maint@the.narro.ws
+    --from maint@the.narro.ws \
+	-v
