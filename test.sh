0851f61cfe7c199f7cc679ae3c489ed81c9abb1a
Matt <brimstone@the.narro.ws>
2015-01-26 22:03:59 -0500

    Adding simple tester.

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+./test.py --ics https://www.google.com/calendar/ical/uq2m73m8lvm2hf86nbfl9g8gkk%40group.calendar.google.com/private-12556f00fa50f0f4a10c2dcf65d7771f/basic.ics \
+	--smtp mail.in.the.narro.ws \
+	--template maint.html \
+    --from maint@the.narro.ws
